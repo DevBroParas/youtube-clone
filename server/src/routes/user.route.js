3f55f89c64ad0609fe29e239f64d7f7fecf8c76d
DevBroParas <parasprofessionaly@gmail.com>
2025-03-27 12:41:05 +0530

    created user route and controller logic

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,24 +1,26 @@
-// import express from "express";
-// import { protect } from "../middleware/auth.js";
+import express from "express";
+import { protect } from "../middleware/auth.js";
+import { uploadAvatar } from "../utils/multer.js";
+import { GetLikedVideos, GetSubscribedChannels, GetUserProfile, SubscribeToChannel, UnsubscribeFromChannel, UpdateUserProfile } from "../controllers/user.controller.js";
 
-// const router = express.Router()
+const router = express.Router()
 
-// //Get user profile
-// router.get("/:userId",)
+//Get user profile
+router.get("/:userId",GetUserProfile)
 
-// //Update user profile
-// router.put("/profile", protect,,)
+//Update user profile
+router.put("/profile", protect,uploadAvatar.single({ name: 'avatar', maxCount: 1 }),UpdateUserProfile)
 
-// //Subscribe to a channel
-// router.post("/:userId/subscribe",protect,)
+//Subscribe to a channel
+router.post("/:userId/subscribe",protect,SubscribeToChannel)
 
-// //Unsubscribe from a channel
-// router.delete("/:userId/unsubscribe",protect,)
+//Unsubscribe from a channel
+router.delete("/:userId/unsubscribe",protect,UnsubscribeFromChannel)
 
-// //Get subscribed channels
-// router.get('/subscriptions',protect,)
+//Get subscribed channels
+router.get('/subscriptions',protect,GetSubscribedChannels)
 
-// //Get liked videos
-// router.get("/liked-videos",protect,)
+//Get liked videos
+router.get("/liked-videos",protect,GetLikedVideos)
 
-// export default router
+export default router
